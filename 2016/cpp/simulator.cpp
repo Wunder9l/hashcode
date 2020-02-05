4f80819b0481a50ac6edc8c6e9e60bb51b7e7672
Wunder9l <vvarty7@gmail.com>
2020-02-05 03:11:07 +0300

    Remove debug output

--- 2016/cpp/simulator.cpp
+++ 2016/cpp/simulator.cpp
@@ -124,7 +124,6 @@ void TSimulator::SetOrders(const vector<int>& ordersIds) {
     unordered_map<int, size_t> positions;
     for (size_t i = 0; i < Orders.size(); ++i){
         positions[Orders[i].id] = i;
-        cout << "positions[" << i << "] = " << i << ", size=" << positions.size() << endl;
     }
     vector<TOrder> newOrders(Orders.size());
     for (size_t i = 0; i < ordersIds.size(); ++i){
